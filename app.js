823770e682adf693d791c9af86bb08c042f4b5fc
TT <tomi.tupiini@gmail.com>
2020-10-07 20:10:55 +0300

    edit app.js, rm GET /reservations2 + related changes to test04

--- app.js
+++ app.js
@@ -187,20 +187,82 @@ app.get('/initdb', async (req, res, next) => {
 });
 
 app.get('/reservations', async (req, res, next) => {
-  console.log('GET /reservations wepa-ht');
+  // GET ALL
+  if (Object.keys(req.query).length === 0) {
+    try {
+      // https://sequelize.org/master/manual/model-querying-basics.html#simple-select-queries
+      // https://sequelize.org/master/manual/eager-loading.html
+      const reservations = await Reservation.findAll({
+        include: [Client, ServiceProvider],
+      });
 
-  try {
-    // https://sequelize.org/master/manual/model-querying-basics.html#simple-select-queries
-    // https://sequelize.org/master/manual/eager-loading.html
-    const reservations = await Reservation.findAll({
-      include: [Client, ServiceProvider],
+      console.log('All reservations:', JSON.stringify(reservations, null, 2));
+      res.status(200).json(reservations); // Content-Type: application/json;
+    } catch (err) {
+      console.log('ERROR from GET /reservations', err);
+      res.status(400).json({ debugMsg: err });
+    }
+  }
+  // SEARCH QUERY
+  else {
+    let okToContinue = true;
+
+    const whereQueryObject = { [Op.and]: [{}] }; // for SELECT WHERE xxx AND xxx conditions
+    const acceptedSearchParams = ['start', 'end', 'spid', 'cid'];
+
+    // any reservation table column can be used as a search param
+    const {
+      start, end, spid, cid,
+    } = req.query;
+
+    Object.keys(req.query).forEach((queryParam) => {
+      if (!acceptedSearchParams.includes(queryParam)) {
+        res.status(400).json({ errorMsg: 'Unaccepted parameter used' });
+        okToContinue = false;
+      }
     });
 
-    console.log('All reservations:', JSON.stringify(reservations, null, 2));
-    res.status(200).json(reservations); // Content-Type: application/json;
-  } catch (err) {
-    console.log('ERROR from GET /reservations', err);
-    res.status(400).json({ debugMsg: err });
+    if (okToContinue) {
+      // if searching with time, both "start" and "end" are needed
+      if ((start && end === undefined) || (start === undefined && end)) {
+        res.status(400).json({ errorMsg: 'Time query needs both start and end values' });
+        okToContinue = false;
+      } else {
+      // https://stackoverflow.com/questions/56169254/how-to-create-dynamic-where-clause-in-sequelize-query-using-or-and-and
+      // https://medium.com/@tinderholmgene/querying-in-sequelize-47393badec01
+      // start and end (time) in query string => add to WHERE
+        whereQueryObject[Op.and][0].start = { [Op.gte]: start };
+        whereQueryObject[Op.and][0].end = { [Op.lte]: end };
+      }
+
+      // serviceproviderid in query string => add to WHERE
+      if (spid !== undefined) {
+        whereQueryObject[Op.and][0] = { serviceproviderid: spid };
+      }
+
+      // customerid in query string => add to WHERE
+      if (cid !== undefined) {
+        whereQueryObject[Op.and][0] = { clientid: cid };
+      }
+    }
+
+    if (okToContinue) {
+      try {
+        // https://sequelize.org/master/manual/model-querying-basics.html#simple-select-queries
+        // https://sequelize.org/master/manual/eager-loading.html
+        const reservations = await Reservation.findAll({
+          include: [Client, ServiceProvider],
+          where: whereQueryObject,
+        });
+
+        console.log('Reservations:', JSON.stringify(reservations, null, 2));
+        res.status(200).json(reservations); // Content-Type: application/json;
+      } catch (err) {
+        console.log('ERROR from GET /reservations2?search_criteria', err);
+        // res.json({ debugMsg: 'Error from GET /reservations2?search_criteria!' });
+        res.status(400).json({ debugMsg: err });
+      }
+    }
   }
 });
 
@@ -289,67 +351,6 @@ app.post('/reservation', async (req, res, next) => {
 
   // https://sequelize.org/master/manual/model-querying-basics.html#applying-where-clauses
   // Check from the reservations if the client or the serviceprovider are booked
-});
-
-app.get('/reservations2', async (req, res, next) => {
-  let okToContinue = true;
-
-  const whereQueryObject = { [Op.and]: [{}] }; // for SELECT WHERE xxx AND xxx conditions
-  const acceptedSearchParams = ['start', 'end', 'spid', 'cid'];
-
-  // any reservation table column can be used as a search param
-  const {
-    start, end, spid, cid,
-  } = req.query;
-
-  Object.keys(req.query).forEach((queryParam) => {
-    if (!acceptedSearchParams.includes(queryParam)) {
-      res.status(400).json({ errorMsg: 'Unaccepted parameter used' });
-      okToContinue = false;
-    }
-  });
-
-  if (okToContinue) {
-    // if searching with time, both "start" and "end" are needed
-    if ((start && end === undefined) || (start === undefined && end)) {
-      res.status(400).json({ errorMsg: 'Time query needs both start and end values' });
-      okToContinue = false;
-    } else {
-    // https://stackoverflow.com/questions/56169254/how-to-create-dynamic-where-clause-in-sequelize-query-using-or-and-and
-    // https://medium.com/@tinderholmgene/querying-in-sequelize-47393badec01
-    // start and end (time) in query string => add to WHERE
-      whereQueryObject[Op.and][0].start = { [Op.gte]: start };
-      whereQueryObject[Op.and][0].end = { [Op.lte]: end };
-    }
-
-    // serviceproviderid in query string => add to WHERE
-    if (spid !== undefined) {
-      whereQueryObject[Op.and][0] = { serviceproviderid: spid };
-    }
-
-    // customerid in query string => add to WHERE
-    if (cid !== undefined) {
-      whereQueryObject[Op.and][0] = { clientid: cid };
-    }
-  }
-
-  if (okToContinue) {
-    try {
-      // https://sequelize.org/master/manual/model-querying-basics.html#simple-select-queries
-      // https://sequelize.org/master/manual/eager-loading.html
-      const reservations = await Reservation.findAll({
-        include: [Client, ServiceProvider],
-        where: whereQueryObject,
-      });
-
-      console.log('Reservations:', JSON.stringify(reservations, null, 2));
-      res.status(200).json(reservations); // Content-Type: application/json;
-    } catch (err) {
-      console.log('ERROR from GET /reservations2?search_criteria', err);
-      // res.json({ debugMsg: 'Error from GET /reservations2?search_criteria!' });
-      res.status(400).json({ debugMsg: err });
-    }
-  }
 });
 
 app.delete('/reservation/:id', async (req, res, next) => {
--- test/04-GET-reservations-searchquery.js
+++ test/04-GET-reservations-searchquery.js
@@ -5,74 +5,74 @@ const server = require('../app')
 
 chai.use(chaiHttp)
 
-describe('GET /reservations2?start&end', () => {
+describe('GET /reservations?start&end', () => {
   it('should return 3 entries with time period 2020-09-29 19:00:00 - 2020-09-30 13:00:00', async () => {
-    const res = await chai.request(server).get('/reservations2?start=2020-09-29 19:00:00&end=2020-09-30 13:00:00')
+    const res = await chai.request(server).get('/reservations?start=2020-09-29 19:00:00&end=2020-09-30 13:00:00')
     /* eslint semi: ["error", "never"] */
     expect(res.status).to.equal(200) // 200 OK
     expect(res.body).to.be.an('array').to.have.lengthOf(3)
   })
   it('should return 2 entries with time period 2020-09-29 19:00:00 - 2020-09-29 21:00:00', async () => {
-    const res = await chai.request(server).get('/reservations2?start=2020-09-29 19:00:00&end=2020-09-29 21:00:00')
+    const res = await chai.request(server).get('/reservations?start=2020-09-29 19:00:00&end=2020-09-29 21:00:00')
     expect(res.status).to.equal(200) // 200 OK
     expect(res.body).to.be.an('array').to.have.lengthOf(2)
   })
   it('should return 1 entries with time period 2020-09-29 19:00:00 - 2020-09-29 20:00:00', async () => {
-    const res = await chai.request(server).get('/reservations2?start=2020-09-29 19:00:00&end=2020-09-29 20:00:00')
+    const res = await chai.request(server).get('/reservations?start=2020-09-29 19:00:00&end=2020-09-29 20:00:00')
     expect(res.status).to.equal(200) // 200 OK
     expect(res.body).to.be.an('array').to.have.lengthOf(1)
   })
   it('should return 0 entries with time period 2020-09-29 15:00:00 - 2020-09-29 15:00:00', async () => {
-    const res = await chai.request(server).get('/reservations2?start=2020-09-29 15:00:00&end=2020-09-29 15:00:00')
+    const res = await chai.request(server).get('/reservations?start=2020-09-29 15:00:00&end=2020-09-29 15:00:00')
     expect(res.status).to.equal(200) // 200 OK
     expect(res.body).to.be.an('array').to.have.lengthOf(0)
   })
   it('should return error if only start search param is used', async () => {
-    const res = await chai.request(server).get('/reservations2?start=2020-09-29 15:00:00')
+    const res = await chai.request(server).get('/reservations?start=2020-09-29 15:00:00')
     expect(res.status).to.equal(400) // 200 OK
     expect(res.body).to.be.an('object')
     expect(res.body).to.have.property('errorMsg')
     expect(res.body.errorMsg).to.equal('Time query needs both start and end values')
   })
   it('should return error if only end search param is used', async () => {
-    const res = await chai.request(server).get('/reservations2?end=2020-09-29 15:00:00')
+    const res = await chai.request(server).get('/reservations?end=2020-09-29 15:00:00')
     expect(res.status).to.equal(400) // 200 OK
     expect(res.body).to.be.an('object')
     expect(res.body).to.have.property('errorMsg')
     expect(res.body.errorMsg).to.equal('Time query needs both start and end values')
   })
   it('should return 3 entries with serviceproviderid 1', async () => {
-    const res = await chai.request(server).get('/reservations2?spid=1')
+    const res = await chai.request(server).get('/reservations?spid=1')
     expect(res.status).to.equal(200) // 200 OK
     expect(res.body).to.be.an('array').to.have.lengthOf(3)
   })
   it('should return 2 entries with serviceproviderid 1 and time period 2020-09-29 19:00:00 - 2020-09-29 21:00:00', async () => {
-    const res = await chai.request(server).get('/reservations2?spid=1&start=2020-09-29 19:00:00&end=2020-09-29 21:00:00')
+    const res = await chai.request(server).get('/reservations?spid=1&start=2020-09-29 19:00:00&end=2020-09-29 21:00:00')
     expect(res.status).to.equal(200) // 200 OK
     expect(res.body).to.be.an('array').to.have.lengthOf(3)
   })
   it('should return 2 entries with clientid 1', async () => {
-    const res = await chai.request(server).get('/reservations2?cid=1')
+    const res = await chai.request(server).get('/reservations?cid=1')
     expect(res.status).to.equal(200) // 200 OK
     expect(res.body).to.be.an('array').to.have.lengthOf(2)
   })
   it('should return 1 entries with clientid 2', async () => {
-    const res = await chai.request(server).get('/reservations2?cid=2')
+    const res = await chai.request(server).get('/reservations?cid=2')
     expect(res.status).to.equal(200) // 200 OK
     expect(res.body).to.be.an('array').to.have.lengthOf(1)
   })
   it('should return 2 entries with clientid 1, serviceproviderid 1, between 2020-2021', async () => {
-    const res = await chai.request(server).get('/reservations2?cid=1&spid=1&start=2020-01-01 00:00:00&end=2021-01-01 00:00:00')
+    const res = await chai.request(server).get('/reservations?cid=1&spid=1&start=2020-01-01 00:00:00&end=2021-01-01 00:00:00')
     expect(res.status).to.equal(200) // 200 OK
     expect(res.body).to.be.an('array').to.have.lengthOf(2)
   })
   it('should return 0 entries with serviceproviderid 9999999', async () => {
-    const res = await chai.request(server).get('/reservations2?spid=9999999')
+    const res = await chai.request(server).get('/reservations?spid=9999999')
     expect(res.status).to.equal(200) // 200 OK
     expect(res.body).to.be.an('array').to.have.lengthOf(0)
   })
   it('should not accept wrong parameters', async () => {
-    const res = await chai.request(server).get('/reservations2?asdf=1&qwer=2')
+    const res = await chai.request(server).get('/reservations?asdf=1&qwer=2')
     expect(res.status).to.equal(400) // 200 OK
     expect(res.body).to.be.an('object')
     expect(res.body).to.have.property('errorMsg')
