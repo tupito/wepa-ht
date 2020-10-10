51daea71049c65eb665c722a0d97612214dbefb3
JGrasscape <janne.ruohoniemi@seamk.fi>
2020-10-10 17:57:32 +0300

    add /controllers/db-helpers.js, moved db-functions from cotrollers.js

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,78 +1,12 @@
 const { Op } = require('sequelize');
 
-// Database connection
-const sequelize = require('../models/sequelize');
-
 // Sequelize models
 const Client = require('../models/client');
 const ServiceProvider = require('../models/serviceprovider');
 const Reservation = require('../models/reservation');
 
-// Testing db connection
-async function testDBConnection() {
-  console.log('Testing DB connection..');
-
-  try {
-    await sequelize.authenticate();
-    console.log('DB connection ok.');
-  } catch (error) {
-    console.error('DB connection nok:', error);
-  }
-}
-
-// Model synchronization to db
-async function synchronizeDBModels() {
-  console.log('Syncing..');
-  // https://sequelize.org/master/manual/model-basics.html#model-synchronization
-  await sequelize.sync({ force: true }) // Force droppaa tablet
-    .then(() => {
-      // Assosisaatiot
-      // https://sequelize.org/master/manual/assocs.html#implementation-3
-      Client.hasMany(Reservation);
-      Reservation.belongsTo(Client);
-      ServiceProvider.hasMany(Reservation);
-      Reservation.belongsTo(ServiceProvider);
-    })
-    .then(() => console.log('All models were synchronized successfully.'))
-    .catch((err) => console.log('ERROR from synchronizeDBModels(): ', err));
-}
-
-// Push example data to db
-async function insertExampleData() {
-  // https://sequelize.org/master/manual/model-instances.html#a-very-useful-shortcut--the--code-create--code--method
-  // Sequelize provides the create method, which combines
-  // the build and save methods shown above into a single method.
-  const client1 = await Client.create({ name: 'L. Palmer' });
-  console.log('Client', client1.name, 'inserted!');
-
-  const client2 = await Client.create({ name: 'D. Cooper' });
-  console.log('Client', client2.name, 'inserted!');
-
-  const serviceProvider = await ServiceProvider.create({ name: 'Dr. Jacoby' });
-  console.log('Service-Provider', serviceProvider.name, 'inserted!');
-
-  // data for db
-  await Reservation.create({
-    start: '2020-09-29 19:00',
-    end: '2020-09-29 20:00',
-    clientid: 1,
-    serviceproviderid: 1,
-  });
-
-  await Reservation.create({
-    start: '2020-09-29 20:01',
-    end: '2020-09-29 21:00',
-    clientid: 2,
-    serviceproviderid: 1,
-  });
-
-  await Reservation.create({
-    start: '2020-09-30 12:00',
-    end: '2020-09-30 13:00',
-    clientid: 1,
-    serviceproviderid: 1,
-  });
-}
+// Database functions
+const db_helper = require('./db-helpers');
 
 // helper functions
 
@@ -102,11 +36,11 @@ function getUndefinedKey(arr, wantedKeys) {
 const getInitDB = async (req, res) => {
   const jsonResponse = [];
   try {
-    await testDBConnection();
+    await db_helper.testDBConnection();
     jsonResponse.push({ debugMsg: 'OK - Tested DB' });
-    await synchronizeDBModels(); // !!! await: cannot insert data before db tables exists
+    await db_helper.synchronizeDBModels(); // !!! await: cannot insert data before db tables exists
     jsonResponse.push({ debugMsg: 'OK - Synchronized models' });
-    await insertExampleData();
+    await db_helper.insertExampleData();
     jsonResponse.push({ debugMsg: 'OK - Inserted example data to DB' });
     res.send(jsonResponse);
   } catch (err) {
---
+++ controllers/db-helpers.js
@@ -0,0 +1,77 @@
+// Database connection
+const sequelize = require('../models/sequelize');
+
+// Sequelize models
+const Client = require('../models/client');
+const ServiceProvider = require('../models/serviceprovider');
+const Reservation = require('../models/reservation');
+
+// Testing db connection
+async function testDBConnection() {
+  console.log('Testing DB connection..');
+
+  try {
+    await sequelize.authenticate();
+    console.log('DB connection ok.');
+  } catch (error) {
+    console.error('DB connection nok:', error);
+  }
+}
+
+// Model synchronization to db
+async function synchronizeDBModels() {
+  console.log('Syncing..');
+  // https://sequelize.org/master/manual/model-basics.html#model-synchronization
+  await sequelize.sync({ force: true }) // Force droppaa tablet
+    .then(() => {
+      // Assosisaatiot
+      // https://sequelize.org/master/manual/assocs.html#implementation-3
+      Client.hasMany(Reservation);
+      Reservation.belongsTo(Client);
+      ServiceProvider.hasMany(Reservation);
+      Reservation.belongsTo(ServiceProvider);
+    })
+    .then(() => console.log('All models were synchronized successfully.'))
+    .catch((err) => console.log('ERROR from synchronizeDBModels(): ', err));
+}
+
+// Push example data to db
+async function insertExampleData() {
+  // https://sequelize.org/master/manual/model-instances.html#a-very-useful-shortcut--the--code-create--code--method
+  // Sequelize provides the create method, which combines
+  // the build and save methods shown above into a single method.
+  const client1 = await Client.create({ name: 'L. Palmer' });
+  console.log('Client', client1.name, 'inserted!');
+
+  const client2 = await Client.create({ name: 'D. Cooper' });
+  console.log('Client', client2.name, 'inserted!');
+
+  const serviceProvider = await ServiceProvider.create({ name: 'Dr. Jacoby' });
+  console.log('Service-Provider', serviceProvider.name, 'inserted!');
+
+  // data for db
+  await Reservation.create({
+    start: '2020-09-29 19:00',
+    end: '2020-09-29 20:00',
+    clientid: 1,
+    serviceproviderid: 1,
+  });
+
+  await Reservation.create({
+    start: '2020-09-29 20:01',
+    end: '2020-09-29 21:00',
+    clientid: 2,
+    serviceproviderid: 1,
+  });
+
+  await Reservation.create({
+    start: '2020-09-30 12:00',
+    end: '2020-09-30 13:00',
+    clientid: 1,
+    serviceproviderid: 1,
+  });
+}
+
+module.exports.testDBConnection = testDBConnection;
+module.exports.synchronizeDBModels = synchronizeDBModels;
+module.exports.insertExampleData = insertExampleData;
